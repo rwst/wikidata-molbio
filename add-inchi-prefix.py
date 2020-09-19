b2af4bbf3d3fe222d85f7099c19bc6567d7e94ce
Ralf Stephan <ralf@ark.in-berlin.de>
2020-09-19 08:28:31 +0200

    bot modules

---
+++ add-inchi-prefix.py
@@ -0,0 +1,32 @@
+
+import os, json, argparse, sys, datetime, time, csv
+
+"""
+"""
+# Initiate the parser
+parser = argparse.ArgumentParser()
+parser.add_argument("-q", "--query", help="perform SPARQL query", 
+        action="store_true")
+
+# Read arguments from the command line
+args = parser.parse_args()
+
+# Check for --version or -V
+dontquery = not args.query
+script = os.path.basename(sys.argv[0])[:-3]
+
+if dontquery is False:
+    ret = os.popen('wd sparql {}.rq >{}.json'.format(script, script))
+    if ret.close() is not None:
+        raise
+
+file = open('{}.json'.format(script))
+s = file.read()
+jol = json.loads(s)
+
+for d in jol:
+    stmt = d.get('stmt')
+    inchi = d.get('inchi')
+#    ref = d.get('ref')
+    print('{} {}'.format(stmt, 'InChi='+inchi))
+    #print('{{ "guid": "{}", "oldValue": "{}", "newValue": "{}" }}'.format(stmt, inchi, 'InChi='+inchi))
