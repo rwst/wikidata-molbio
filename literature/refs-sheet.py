2680624b582568bc6c67b94afabe61830bb3ea66
Ralf Stephan <ralf@ark.in-berlin.de>
2021-07-03 19:33:07 +0200

    update

--- literature/refs-sheet.py
+++ literature/refs-sheet.py
@@ -1,5 +1,5 @@
 
-import csv, json, argparse, os, sys
+import csv, json, argparse, os, sys, gc
 from sys import *
 
 def Len(obj):
@@ -8,13 +8,13 @@ def Len(obj):
     else:
         return len(obj)
 
-def add_to_slot(prot,data,title,pdate,pmid,pmcid,doi,rev):
+def add_to_slot(prot,data,title,pdate,pmid,pmcid,doi,foll,rev):
     l = data.get(prot)
     if l is None:
-        l = set([(title,pdate,pmid,pmcid,doi,rev)])
+        l = set([(title,pdate,pmid,pmcid,doi,foll,rev)])
         data[prot] = l
     else:
-        l.add((title,pdate,pmid,pmcid,doi,rev))
+        l.add((title,pdate,pmid,pmcid,doi,foll,rev))
 8
 # Initiate the parser
 parser = argparse.ArgumentParser()
@@ -52,6 +52,7 @@ for item in jol:
     pmid = item.get('pmid')
     pmcid = item.get('pmcid')
     doi = item.get('doi')
+    foll = item.get('foll')
     rev = item.get('rev')
     is_added = False
     if Len(rev) > 0:
@@ -61,29 +62,49 @@ for item in jol:
     prot = item.get('protLabel')
     if prot == 'replication/transcription complex':
         prot = 'RTC'
-    if Len(prot) > 0:
-        is_added = True
-        prots.add(prot)
-        add_to_slot(prot,data,title,pdate,pmid,pmcid,doi,rev)
-    for topic in ['autoi', 'fusion', 'omics', 'rna', 'rev']:
+    # exclusive topics
+    for topic in ['fusion', 'glyc']:
         if Len(item.get(topic)) > 0:
             is_added = True
-            add_to_slot(topic,data,title,pdate,pmid,pmcid,doi,rev)
+            add_to_slot(topic,data,title,pdate,pmid,pmcid,doi,foll,rev)
             prots.add(topic)
+    if not is_added and Len(prot) > 0:
+        is_added = True
+        prots.add(prot)
+        add_to_slot(prot,data,title,pdate,pmid,pmcid,doi,foll,rev)
+    # inclusive topics
+    for topic in ['autoi', 'omics', 'rna', 'rev']:
+        if Len(item.get(topic)) > 0:
+            is_added = True
+            add_to_slot(topic,data,title,pdate,pmid,pmcid,doi,foll,rev)
+            prots.add(topic)
+    if item.get('top') == 'Q128406':
+        is_added = True
+        prot = 'drugs'
+        prots.add(prot)
+        add_to_slot(prot,data,title,pdate,pmid,pmcid,doi,foll,rev)
     if not is_added and item.get('top') == 'Q7202':
         prot = 'Misc'
         prots.add(prot)
-        add_to_slot(prot,data,title,pdate,pmid,pmcid,doi,rev)
+        add_to_slot(prot,data,title,pdate,pmid,pmcid,doi,foll,rev)
+
+ret = os.popen('rm -f refs-*.csv')
 
 for p in prots:
     with open('refs-{}.csv'.format(p), 'w', newline='') as csvfile:
         fieldnames = ['Title', 'Rev', 'Published', 'PMID', 'PMC', 'DOI']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         writer.writeheader()
-        if data.get(p) is None:
+        dataset = data.get(p)
+        if dataset is None:
+            print('Closed {}'.format(p))
+            csvfile.close()
             continue
-        print('writing {}'.format(p), flush=True, file=stderr)
-        for title,pdate,pmid,pmcid,doi,rev in sorted(data.get(p), key=lambda tup : tup[1], reverse=True):
+        dois = set([doi for title,pdate,pmid,pmcid,doi,foll,rev in dataset])
+        dataset = set([(title,pdate,pmid,pmcid,doi,foll,rev) for title,pdate,pmid,pmcid,doi,foll,rev in dataset if Len(foll)==0 or foll not in dois])
+        sortedset = sorted(dataset, key=lambda tup : tup[1], reverse=True)
+        print('writing "{}" ({})'.format(p, len(sortedset)), flush=True)
+        for title,pdate,pmid,pmcid,doi,foll,rev in sortedset:
             if Len(pmid) > 0:
                 pmid = "https://pubmed.ncbi.nlm.nih.gov/"+pmid
             else:
@@ -100,6 +121,11 @@ for p in prots:
                 d = {'Title':title, 'Rev':rev, 'Published':pdate, 'PMID':pmid, 'PMC':pmcid, 'DOI':doi }
             except TypeError:
                 print(title)
-                exit()
-            writer.writerow(d)
+                raise
+            ret = writer.writerow(d)
+            #print("{}: {}".format(ret, d), flush=True)
+        del writer
+        gc.collect()
+        csvfile.close()
 
+print(len(prots))
