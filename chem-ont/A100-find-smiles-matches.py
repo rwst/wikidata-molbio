d26247867eb7d9d4d183e91c05780b73b0ba797d
Ralf Stephan <ralf@ark.in-berlin.de>
2021-08-01 08:36:33 +0200

    bot modules

---
+++ chem-ont/A100-find-smiles-matches.py
@@ -0,0 +1,67 @@
+import csv, os, json, argparse, sys, re
+from rdkit import Chem
+
+"""
+Print tree of proper chemclasses
+"""
+# Initiate the parser
+parser = argparse.ArgumentParser()
+parser.add_argument("-q", "--query", help="perform SPARQL query",
+        action="store_true")
+parser.add_argument("-p", "--pattern", help="SMARTS to match",
+        required=True)
+parser.add_argument("-i", "--ident", help="report identical SMILES only",
+        action="store_true")
+
+# Read arguments from the command line
+args = parser.parse_args()
+#print(args)
+
+# Check for --version or -V [] \,\$
+dontquery = not args.query
+pat = Chem.MolFromSmarts(args.pattern)
+script = os.path.basename(sys.argv[0])[:-3]
+if args.ident:
+    idpat = Chem.MolFromSmiles(args.pattern)
+    idsm = Chem.MolToSmiles(idpat, isomericSmiles=False)
+    idstereo = len(Chem.FindPotentialStereo(idpat))
+    print('{}'.format(idstereo))
+
+reader = csv.DictReader(open('smiles.tsv', 'r'), delimiter='\t')
+labels = {}
+smiles = {}
+for d in reader:
+    iturl = d.get('item')
+    it = iturl[iturl.rfind('/')+1:] 
+    lab = d.get('itemLabel')
+    labels[it] = lab
+    #smiles[it] = d.get('smiles')
+
+c = 0
+reader = csv.DictReader(open('inchi.tsv', 'r'), delimiter='\t')
+for d in reader:
+#    c = c + 1
+#    if (c % 10000) == 0:
+#        print(c)
+    iturl = d.get('item')
+    it = iturl[iturl.rfind('/')+1:]
+    inchi = d.get('inchi')
+    s = smiles.get(it)
+    if s is not None:
+        mol = Chem.MolFromSmiles(s)
+    else:
+        mol = Chem.MolFromInchi(inchi)
+    if mol is None:
+        continue
+    if args.ident:
+        sm = Chem.MolToSmiles(mol, isomericSmiles=False)
+        #print('{} {}'.format(it, sm))
+        if sm == idsm:
+            ism = Chem.MolToSmiles(mol)
+            nst = len(re.findall('@@?', ism))
+            print('{} {} {}'.format(idstereo-nst, it, labels.get(it)))
+        continue
+    hits = mol.GetSubstructMatches(pat)
+    if len(hits) > 3:
+        if Chem.MolToSmiles(mol).count('C') == len(hits):
+            print('{} {} {} {}'.format(it, labels.get(it), len(hits), Chem.MolToSmiles(mol).count('C')))
